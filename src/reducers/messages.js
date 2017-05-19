2e6e2ec8fb60d0d5ce74e8c14bcdd835463df273
NMVikings <no.more.vikings@gmail.com>
2017-05-19 09:07:40 +0300

    Change MessageData structure, remake structure of state.messages

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,49 +1,29 @@
 import { ADD_MESSAGE, EDIT_MESSAGE, ADD_MESSAGE_LIST } from '../actions';
 
 const processMessagesList = (data) => {
-  const processDate = ({ date }) => {
-    return date.split('.').reverse()
-      .map((item, index) => {
-        if (index === 1) {
-          return String(Number(item) - 1)
-        }
-        return item;
-      });
+  const processDate = (newData, item) => {
+    const fullDate = new Date(item.date).toLocaleDateString();
+    if (newData[fullDate] === undefined) {
+      return {...newData, [fullDate]: [item]}
+    } else {
+      return {...newData, [fullDate]: [...newData[fullDate], item]}
+    }
   };
 
-  const processTime = ({ time }) => {
-    return ['1970', '0', '1', ...time.split(':')];
-  };
-
-  const dataFilter = (obj1, obj2) => {
-    const date1 = new Date(...processDate(obj1));
-    const date2 = new Date(...processDate(obj2));
+  const sortByDate = (data1, data2) => {
+    const date1 = new Date(data1.date);
+    const date2 = new Date(data2.date);
 
     if (date1 > date2) {
       return -1;
     }
-
     if (date1 < date2) {
-      return 1
-    }
-
-    return 0;
-  };
-
-  const sortByTime = (message1, message2) => {
-    const time1 = new Date(...processTime(message1)).getTime();
-    const time2 = new Date(...processTime(message2)).getTime();
-
-    if (time1 > time2) {
-      return -1;
-    }
-    if (time1 < time2) {
       return 1;
     }
     return 0;
   };
 
-  return data.sort(dataFilter).map((item) => item.messages.sort(sortByTime));
+  return data.sort(sortByDate).reduce(processDate, {});
 };
 
 const messageReducer = (state = {}, action) => {
