bdcd98d3c01a365d4b35b56d53d3439be659478c
NMVikings <no.more.vikings@gmail.com>
2017-05-21 15:19:01 +0300

    Add react-router, configure for default page

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import App from './App/App';
 import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory'
+
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router history={createBrowserHistory()}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>
 );
 
