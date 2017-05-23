1ebf929008048799173196f44cbe3d8a7d41c6f3
NMVikings <no.more.vikings@gmail.com>
2017-05-23 11:47:09 +0300

    Fix data and add currency for operation

--- src/components/Operation/Operation.js
+++ src/components/Operation/Operation.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Operation.css';
 
-const Operation = ({ date, text, change, moneyDirection }) => {
+const Operation = ({ date, text, change, moneyDirection, currency }) => {
   const classString =`change ${moneyDirection ? 'change_positive' : 'change_negative'}`;
   return (
     <div className="operation">
       <span className="operation__date">{date.toLocaleDateString()} | {date.toLocaleTimeString()}</span>
       <br/>
-      {text}  <span className={classString}>{moneyDirection ? '+' : '-'} {change} </span>
+      {text}  <span className={classString}>{moneyDirection ? '+' : '-'} {change} {currency}</span>
     </div>
   )
 };
